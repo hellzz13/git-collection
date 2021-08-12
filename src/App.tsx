722bee90d5146952d47a5b00343af3ba6b50d4c8
hellzz13 <hpalenca13@gmail.com>
2021-08-12 10:08:31 -0300

    Config global style

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import { Routes } from './routes';
 
+import { GlobalStyle } from './styles/global'
+
 
 
 const App: React.FC = () => {
   return (
-
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+      <GlobalStyle />
+    </>
   );
 }
 

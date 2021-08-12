1fa730e4c24f3135e2d2217d8a47b3a7b84bbc2e
hellzz13 <hpalenca13@gmail.com>
2021-08-12 14:07:46 -0300

    final layout dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import { Title } from './styles';
-
+import { Title, Form, Repos } from './styles';
+import logo from '../../assets/logo.svg'
+import { FiChevronRight } from 'react-icons/fi';
 
 export const Dashboard: React.FC = () => {
     return (
-        <Title>Dashboard</Title>
+        <>
+            <img src={logo} alt="Git-Collection" />
+            <Title>Github Catalog</Title>
+            <Form>
+                <input placeholder="Name / Repository Name" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+            <Repos>
+                <a href="/repositories">
+                    <img src="https://github.com/hellzz13.png" alt="Repositório" />
+                    <div>
+                        <strong>repositorio/repositório</strong>
+                        <p>repositírio do curso</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+
+            </Repos>
+        </>
+
     );
 }
 

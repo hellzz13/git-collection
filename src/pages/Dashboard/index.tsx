3d9ccb71d5388e9d67cfada1b235cae173830501
hellzz13 <hpalenca13@gmail.com>
2021-08-13 13:46:59 -0300

    import public API github

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 import { Title, Form, Repos } from './styles';
 import logo from '../../assets/logo.svg'
 import { FiChevronRight } from 'react-icons/fi';
+import { api } from '../../services/api'
 
+interface GithubRepository {
+    full_name: string,
+    description: string,
+    owner: {
+        login: string,
+        avatar_url: string,
+    };
+}
 export const Dashboard: React.FC = () => {
     return (
         <>
@@ -16,7 +25,7 @@ export const Dashboard: React.FC = () => {
                 <a href="/repositories">
                     <img src="https://github.com/hellzz13.png" alt="Repositório" />
                     <div>
-                        <strong>repositorio/repositório</strong>
+                        <strong>hellzz13/git-collection</strong>
                         <p>repositírio do curso</p>
                     </div>
                     <FiChevronRight size={20} />

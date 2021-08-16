3f3ba05ecf8497ca47b23bc44c3b0e529f27ded2
hellzz13 <hpalenca13@gmail.com>
2021-08-16 12:01:11 -0300

    error validation

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,61 +1,71 @@
 import React from "react";
-import { Title, Form, Repos } from "./styles";
+import { Title, Form, Repos, Error } from "./styles";
 import logo from "../../assets/logo.svg";
 import { FiChevronRight } from "react-icons/fi";
 import { api } from "../../services/api";
 
 interface GithubRepository {
-  full_name: string;
-  description: string;
-  owner: {
-    login: string;
-    avatar_url: string;
-  };
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
 }
 export const Dashboard: React.FC = () => {
-  const [repos, setRepos] = React.useState<GithubRepository[]>([]); //armazena a lista
-  const [newRepo, setNewRepo] = React.useState(""); // armazena o input
+    const [repos, setRepos] = React.useState<GithubRepository[]>([]); //armazena a lista
+    const [newRepo, setNewRepo] = React.useState(""); // armazena o input
+    const [inputError, setInputError] = React.useState('');
 
-  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
-    setNewRepo(event.target.value);
-  }
-  async function handleAddRepo(
-    event: React.FormEvent<HTMLFormElement>
-  ): Promise<void> {
-    event.preventDefault();
-    const response = await api.get<GithubRepository>(`repos/${newRepo}`);
+    function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
+        setNewRepo(event.target.value);
+    }
+    async function handleAddRepo(
+        event: React.FormEvent<HTMLFormElement>
+    ): Promise<void> {
+        event.preventDefault();
 
-    const repository = response.data;
+        if (!newRepo) {
+            setInputError("Informe o username / repositório")
+            return;
+        }
 
-    setRepos([...repos, repository]);
-    setNewRepo(" ");
-  }
-  return (
-    <>
-      <img src={logo} alt="Git-Collection" />
-      <Title>Github Catalog</Title>
-      <Form onSubmit={handleAddRepo}>
-        <input
-          placeholder="Name / Repository Name"
-          onChange={handleInputChange}
-        />
-        <button type="submit">Pesquisar</button>
-      </Form>
-      <Repos>
-        {repos.map((repository) => (
-          <a href="/repositories" key={repository.full_name}>
-            <img
-              src={repository.owner.avatar_url}
-              alt={repository.owner.login}
-            />
-            <div>
-              <strong>{repository.full_name}</strong>
-              <p>{repository.description}</p>
-            </div>
-            <FiChevronRight size={20} />
-          </a>
-        ))}
-      </Repos>
-    </>
-  );
+        const response = await api.get<GithubRepository>(`repos/${newRepo}`);
+
+        const repository = response.data;
+
+        setRepos([...repos, repository]);
+        setNewRepo("");
+    }
+    return (
+        <>
+            <img src={logo} alt="Git-Collection" />
+            <Title>Github Catalog</Title>
+            <Form hasError={Boolean(inputError)} onSubmit={handleAddRepo}>
+                <input
+                    placeholder="Name / Repository Name"
+                    onChange={handleInputChange}
+                />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            {inputError && <Error>{inputError}</Error>}
+
+            <Repos>
+                {repos.map((repository) => (
+                    <a href="/repositories" key={repository.full_name}>
+                        <img
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login}
+                        />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                        <FiChevronRight size={20} />
+                    </a>
+                ))}
+            </Repos>
+        </>
+    );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,10 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import { shade } from 'polished';
+
+interface FormProps {
+    hasError: boolean;
+}
 
 export const Title = styled.h1`
     font-size: 48px;
@@ -11,7 +15,7 @@ export const Title = styled.h1`
 
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
     margin-top: 40px;
     max-width: 700px;
     display: flex;
@@ -24,6 +28,10 @@ export const Form = styled.form`
         border-radius: 5px 0px 0px 5px;
         color: #3a3a3a;
         border-right: 0;
+
+        ${(props) => props.hasError && css`
+            border-color: #c53030;
+        ` }
 
         &::placeholder{
             color: #a8a8b3;
@@ -45,7 +53,7 @@ export const Form = styled.form`
         
     }
     
-`
+`;
 export const Repos = styled.div`
     margin-top: 80px;
     max-width: 700px;
@@ -89,5 +97,12 @@ export const Repos = styled.div`
             color: #cbcbd6;
         }
     }
+
+`;
+
+export const Error = styled.span`
+    display: block;
+    color: #c53030;
+    margin-top: 8px;
 
 `;

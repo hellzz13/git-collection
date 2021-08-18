b3a0096971f235c03e2c3ad74ad35d1668944ab8
hellzz13 <hpalenca13@gmail.com>
2021-08-18 10:20:40 -0300

    end of repo page

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -11,17 +11,40 @@ interface RepositoryParams {
 
 }
 
+interface GithubRepository {
+    full_name: string;
+    description: string;
+    forks_count: number;
+    open_issues_count: number;
+    stargazers_count: number;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+}
+
+interface GithubIssue {
+    id: number;
+    title: string;
+    html_url: string;
+    user: {
+        login: string;
+    }
+}
+
 export const Repo: React.FC = () => {
 
     const { params } = useRouteMatch<RepositoryParams>()
+    const [repository, setRepository] = React.useState<GithubRepository | null>(null)
+    const [issues, setIssues] = React.useState<GithubIssue[]>([])
 
     React.useEffect(() => {
         api
             .get(`repos/${params.repository}`)
-            .then(response => console.log(response.data))
+            .then(response => setRepository(response.data))
         api
             .get(`repos/${params.repository}/issues`)
-            .then(response => console.log(response.data))
+            .then(response => setIssues(response.data))
 
     }, [params.repository]);
 
@@ -37,37 +60,44 @@ export const Repo: React.FC = () => {
                 </Link>
 
             </Header>
-            <RepoInfo>
-                <header>
-                    <img src="http://github.com/hellzz13.png" alt="Teste" />
-                    <div>
-                        <strong>hellzz13/Churrascometro</strong>
-                        <p>Repositorio churrascometro</p>
-                    </div>
-                </header>
-                <ul>
-                    <li>
-                        <strong>2333</strong>
-                        <span>Stars</span>
-                    </li>
-                    <li>
-                        <strong>23</strong>
-                        <span>Forks</span>
-                    </li>
-                    <li>
-                        <strong>3</strong>
-                        <span>Issues abertas</span>
-                    </li>
-                </ul>
-            </RepoInfo>
+
+            {repository && (
+                <RepoInfo>
+                    <header>
+                        <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                    </header>
+                    <ul>
+                        <li>
+                            <strong>{repository.stargazers_count}</strong>
+                            <span>Stars</span>
+                        </li>
+                        <li>
+                            <strong>{repository.forks_count}</strong>
+                            <span>Forks</span>
+                        </li>
+                        <li>
+                            <strong>{repository.open_issues_count}</strong>
+                            <span>Issues abertas</span>
+                        </li>
+                    </ul>
+                </RepoInfo>
+            )}
+
             <Issues>
-                <Link to='/'>
-                    <div>
-                        <strong>REPOSITORIO AQUI</strong>
-                        <p>descrição aqui</p>
-                    </div>
-                    <FiChevronRight size={20} />
-                </Link>
+                {issues.map(issue => (
+                    <a href={issue.html_url} key={issue.id}>
+                        <div>
+                            <strong>{issue.title}</strong>
+                            <p>{issue.user.login}</p>
+                        </div>
+                        <FiChevronRight size={20} />
+                    </a>
+                ))}
+
             </Issues>
         </>
     );
